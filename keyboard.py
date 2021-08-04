c13d67badb203cde823fd046f0fc237ef7c4354b
Muhammad Al-Barham <mohammedbrham98@gmail.com>
2021-08-05 02:53:57 +0300

    Add display code

--- keyboard.py
+++ keyboard.py
@@ -1,33 +1,68 @@
 import pygame
 
 clock = pygame.time.Clock()
+# win = pygame.display.set_mode((100,100))
+# define the RGB value
+# for white colour
+white = (255, 255, 255)
+
+# assigning values to X and Y variable
+X = 700
+Y = 700
+
+# create the display surface object
+# of specific dimension..e(X, Y).
+display_surface = pygame.display.set_mode((X, Y))
+
+# set the pygame window name
+pygame.display.set_caption("Image")
+
+# create a surface object, image is drawn on it.
+image1 = pygame.image.load(r"/home/pi/Desktop/ArSL_proj/download.jpeg")
+image2 = pygame.image.load(r"/home/pi/Desktop/ArSL_proj/Beh.jpeg")
+
 
 def init():
     pygame.init()
-    win = pygame.display.set_mode((100,100))
+    display_surface.fill(white)
+
 
 def getKey(KeyName):
 
     ans = False
     clock.tick(10)
-    for eve in pygame.event.get():pass
+    for eve in pygame.event.get():
+        pass
     keyInput = pygame.key.get_pressed()
-	
-    myKey = getattr(pygame, 'K_{}'.format(KeyName))
-    if keyInput [myKey]:
+
+    myKey = getattr(pygame, "K_{}".format(KeyName))
+    if keyInput[myKey]:
         ans = True
+
     pygame.display.update()
+
     return ans
 
+
 def main():
+    # completely fill the surface object
+    # with white colour:
 
-    if getKey('a'):
-        print('Key a was pressed')
-    if getKey('b'):
-        print('Key b was pressed')
+    # copying the image surface object
+    # to the display surface object at
+    # (0, 0) coordinate.
+
+    if getKey("a"):
+        print("Key a was pressed")
+        display_surface.fill(white)
+        display_surface.blit(image1, (0, 0))
+    if getKey("b"):
+        print("Key b was pressed")
+        display_surface.fill(white)
+        display_surface.blit(image2, (0, 0))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     init()
     while True:
         main()
